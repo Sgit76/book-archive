0815b50549d33ba0257acfce8d0153396a8c44c0
Sgit76 <shafiul.cse76@gmail.com>
2021-09-08 11:24:54 +0600

    final

--- js/script.js
+++ js/script.js
@@ -1,6 +1,10 @@
-//Took input value and process with button 
+//Took input value and process with button
+const toggleSpinner = displayStyle => {
+    document.getElementById('spinner').style.display = displayStyle;
+}
 const searchBook = () =>{
     const searchField = document.getElementById('search-field');
+    toggleSpinner('block');
     const searchText = searchField.value;
     // console.log(searchText);
     searchField.value = '';
@@ -8,5 +12,32 @@ const searchBook = () =>{
     const url = `https://openlibrary.org/search.json?q=${searchText}`;
     fetch(url)
     .then(res => res.json())
-    .then(data => console.log(data))
+    .then(data => displaySearchResult(data));
 } 
+
+const displaySearchResult = books =>{
+   console.log(books);
+   
+    const searchTotal = document.getElementById('search-total');
+    const totalDiv = document.createElement('div');
+        totalDiv.innerHTML = `
+            <p class="text-center">Total Books: ${books.numFound} </p>
+        `;
+        searchTotal.appendChild(totalDiv);
+        
+    const searchResult = document.getElementById('search-result');
+        const div = document.createElement('div');
+        div.classList.add('col');
+        div.innerHTML = `
+        <div class="card h-100">
+        <img src="https://covers.openlibrary.org/b/id/${books.docs[0].cover_i}-M.jpg" class="card-img-top" alt="...">
+            <div class="card-body">
+              <p class="card-title">Book name: ${books.docs[0].title}</p>
+              <p class="card-text">Author name: ${books.docs[0].author_name}</p>
+              <p class="card-text">First published: ${books.docs[0].publish_date[0]}</p>
+            </div>
+        </div>
+        `;
+        searchResult.appendChild(div);
+        toggleSpinner('none');
+}
